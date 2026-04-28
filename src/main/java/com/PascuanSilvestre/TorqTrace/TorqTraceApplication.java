e76b7c4767fd3670f9741f90ebcd18d192d0f881
JoeL2481 <92615987+JoeL2481@users.noreply.github.com>
2026-04-28 09:53:52 -0300

    First commit

---
+++ src/main/java/com/PascuanSilvestre/TorqTrace/TorqTraceApplication.java
@@ -0,0 +1,13 @@
+package com.PascuanSilvestre.TorqTrace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorqTraceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorqTraceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PascuanSilvestre/TorqTrace/TorqTraceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PascuanSilvestre.TorqTrace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorqTraceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
