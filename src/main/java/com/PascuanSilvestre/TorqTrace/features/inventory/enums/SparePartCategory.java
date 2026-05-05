b67af7689599f8888e67fb39f5363542c3b4d3c5
JoeL2481 <joel248@hotmail.com.ar>
2026-05-05 00:32:10 -0300

    feat: add spare part compatibility and inventory model

---
+++ src/main/java/com/PascuanSilvestre/TorqTrace/features/inventory/enums/SparePartCategory.java
@@ -0,0 +1,14 @@
+package com.PascuanSilvestre.TorqTrace.features.inventory.enums;
+
+public enum SparePartCategory {
+    ENGINE,
+    SUSPENSION,
+    BRAKES,
+    ELECTRICAL,
+    FILTERS,
+    ACCESSORY,
+    TRANSMISSION,
+    COOLING,
+    EXHAUST,
+    BODY
+}
