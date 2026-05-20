b80b3a9e5aca612d1fc0472aedcddb7c18ce0395
Geronimo Silvestre <geronimosilvestre@hotmail.com>
2026-05-20 01:15:32 -0300

    create all workShop controllers and workOrder controllers

--- src/main/java/com/PascuanSilvestre/TorqTrace/features/auth/authProvider/AuthProviderController.java
+++ src/main/java/com/PascuanSilvestre/TorqTrace/features/auth/authProvider/AuthProviderController.java
@@ -1,4 +1,37 @@
 package com.PascuanSilvestre.TorqTrace.features.auth.authProvider;
 
+import com.PascuanSilvestre.TorqTrace.features.auth.authProvider.dto.AuthProviderCreateDTO;
+import com.PascuanSilvestre.TorqTrace.features.auth.authProvider.dto.AuthProviderResponseDTO;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestControllerAdvice
+@RequestMapping("api/authprovider")
+@AllArgsConstructor
+
 public class AuthProviderController {
+
+     private final AuthProviderService  authProviderService;
+
+     @PostMapping
+    public ResponseEntity<AuthProviderResponseDTO> create(@Valid @RequestBody AuthProviderCreateDTO request) {
+         return ResponseEntity.ok(authProviderService.create(request));
+     }
+     @GetMapping
+    public ResponseEntity<List<AuthProviderResponseDTO>> findAll() {
+         return ResponseEntity.ok(authProviderService.getAll());
+     }
+     @GetMapping("/{id}")
+    public ResponseEntity<AuthProviderResponseDTO> findById(@Valid @PathVariable("id") Long id) {
+         return ResponseEntity.ok(authProviderService.getById(id));
+     }
+     @DeleteMapping("/{id}")
+    public ResponseEntity<AuthProviderResponseDTO> delete(@Valid @PathVariable("id") Long id) {
+         return ResponseEntity.ok(authProviderService.delete(id));
+     }
+
 }
--- src/main/java/com/PascuanSilvestre/TorqTrace/features/auth/userProvider/UserProviderController.java
+++ src/main/java/com/PascuanSilvestre/TorqTrace/features/auth/userProvider/UserProviderController.java
@@ -1,4 +1,36 @@
 package com.PascuanSilvestre.TorqTrace.features.auth.userProvider;
 
+import com.PascuanSilvestre.TorqTrace.features.auth.userProvider.dto.UserProviderCreateDTO;
+import com.PascuanSilvestre.TorqTrace.features.auth.userProvider.dto.UserProviderResponseDTO;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/userprovider")
+@AllArgsConstructor
 public class UserProviderController {
+    private final UserProviderService userProviderService;
+
+    @PostMapping
+    public ResponseEntity<UserProviderResponseDTO> save(@Valid @RequestBody UserProviderCreateDTO request) {
+        return ResponseEntity.ok(userProviderService.create(request));
+    }
+    @GetMapping
+    public ResponseEntity<List<UserProviderResponseDTO>>findAll(){
+        return ResponseEntity.ok(userProviderService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserProviderResponseDTO> getById(@PathVariable Long id){
+        return ResponseEntity.ok(userProviderService.getById(id));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<UserProviderResponseDTO> deleteById(@PathVariable Long id){
+        return ResponseEntity.ok(userProviderService.delete(id));
+    }
 }
