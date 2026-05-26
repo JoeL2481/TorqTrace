591aa5e261b0afd90aef57f6040f9e6c9baaf7cb
JoeL2481 <joel248@hotmail.com.ar>
2026-05-26 03:04:51 -0300

    Added functional crud for transmission

--- src/main/java/com/PascuanSilvestre/TorqTrace/features/vehicle/vehiclePowerTrain/transmission/TransmissionController.java
+++ src/main/java/com/PascuanSilvestre/TorqTrace/features/vehicle/vehiclePowerTrain/transmission/TransmissionController.java
@@ -1,4 +1,48 @@
 package com.PascuanSilvestre.TorqTrace.features.vehicle.vehiclePowerTrain.transmission;
 
+import com.PascuanSilvestre.TorqTrace.features.vehicle.vehiclePowerTrain.transmission.dto.TransmissionCreateDTO;
+import com.PascuanSilvestre.TorqTrace.features.vehicle.vehiclePowerTrain.transmission.dto.TransmissionResponseDTO;
+import com.PascuanSilvestre.TorqTrace.features.vehicle.vehiclePowerTrain.transmission.dto.TransmissionUpdateDTO;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/transmission")
+@RequiredArgsConstructor
 public class TransmissionController {
+
+    private final TransmissionService service;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TransmissionResponseDTO create(@Valid @RequestBody TransmissionCreateDTO request) {
+        return service.create(request);
+    }
+
+    @GetMapping("/{id}")
+    public TransmissionResponseDTO getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<TransmissionResponseDTO> getAll() {
+        return service.getAll();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public TransmissionResponseDTO update(@PathVariable Long id, @Valid @RequestBody TransmissionUpdateDTO request) {
+        return service.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public TransmissionResponseDTO delete(@PathVariable Long id) {
+        return service.delete(id);
+    }
 }
--- src/main/java/com/PascuanSilvestre/TorqTrace/features/vehicle/vehiclePowerTrain/transmission/TransmissionService.java
+++ src/main/java/com/PascuanSilvestre/TorqTrace/features/vehicle/vehiclePowerTrain/transmission/TransmissionService.java
@@ -1,4 +1,59 @@
 package com.PascuanSilvestre.TorqTrace.features.vehicle.vehiclePowerTrain.transmission;
 
-public class TransmissionService {
+import com.PascuanSilvestre.TorqTrace.common.ICrudServiceComplete;
+import com.PascuanSilvestre.TorqTrace.features.vehicle.vehiclePowerTrain.transmission.dto.TransmissionCreateDTO;
+import com.PascuanSilvestre.TorqTrace.features.vehicle.vehiclePowerTrain.transmission.dto.TransmissionResponseDTO;
+import com.PascuanSilvestre.TorqTrace.features.vehicle.vehiclePowerTrain.transmission.dto.TransmissionUpdateDTO;
+import com.PascuanSilvestre.TorqTrace.features.vehicle.vehiclePowerTrain.transmission.mapper.TransmissionMapper;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TransmissionService implements ICrudServiceComplete<TransmissionCreateDTO, TransmissionUpdateDTO, TransmissionResponseDTO, Long> {
+
+    private final TransmissionRepository repo;
+    private final TransmissionMapper mapper;
+
+    @Override
+    public TransmissionResponseDTO create(TransmissionCreateDTO request) {
+        TransmissionEntity entity = mapper.toEntity(request);
+        return mapper.toResponse(repo.save(entity));
+    }
+
+    @Override
+    public List<TransmissionResponseDTO> getAll() {
+        return repo.findAll()
+                .stream()
+                .map(mapper::toResponse)
+                .toList();
+    }
+
+    public TransmissionEntity getEntityById(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Transmission not found for id: " + id));
+    }
+
+    @Override
+    public TransmissionResponseDTO getById(Long id) {
+        return mapper.toResponse(getEntityById(id));
+    }
+
+    @Override
+    public TransmissionResponseDTO update(Long id, TransmissionUpdateDTO request) {
+        TransmissionEntity entity = getEntityById(id);
+        mapper.toEntityUpdate(request, entity);
+        return mapper.toResponse(repo.save(entity));
+    }
+
+    @Override
+    public TransmissionResponseDTO delete(Long id) {
+        TransmissionEntity entity = getEntityById(id);
+        TransmissionResponseDTO dto = mapper.toResponse(entity);
+        repo.delete(entity);
+        return dto;
+    }
 }
