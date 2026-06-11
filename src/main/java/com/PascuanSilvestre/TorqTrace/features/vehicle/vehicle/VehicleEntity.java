eca6bdd09e414480d0345ee2a1de5ee36ee11c52
JoeL2481 <92615987+JoeL2481@users.noreply.github.com>
2026-06-11 11:57:59 -0300

    vehicle logic changes, new cardinality added, and dto can receive id or string

--- src/main/java/com/PascuanSilvestre/TorqTrace/features/vehicle/vehicle/VehicleEntity.java
+++ src/main/java/com/PascuanSilvestre/TorqTrace/features/vehicle/vehicle/VehicleEntity.java
@@ -1,6 +1,7 @@
 package com.PascuanSilvestre.TorqTrace.features.vehicle.vehicle;
 
 import com.PascuanSilvestre.TorqTrace.common.utils.AuditableBase;
+import com.PascuanSilvestre.TorqTrace.features.userVehicle.userVehicle.UserVehicleEntity;
 import com.PascuanSilvestre.TorqTrace.features.vehicle.vehicleCatalog.enums.VehicleBodyType;
 import com.PascuanSilvestre.TorqTrace.features.vehicle.vehicleCatalog.enums.VehicleCategory;
 import com.PascuanSilvestre.TorqTrace.features.vehicle.vehicleCatalog.vehicleBrand.VehicleBrandEntity;
@@ -8,6 +9,8 @@ import com.PascuanSilvestre.TorqTrace.features.vehicle.vehicleCatalog.vehicleEqu
 import com.PascuanSilvestre.TorqTrace.features.vehicle.vehicleCatalog.vehicleGeneration.VehicleGenerationEntity;
 import com.PascuanSilvestre.TorqTrace.features.vehicle.vehicleCatalog.vehicleModel.VehicleModelEntity;
 import com.PascuanSilvestre.TorqTrace.features.vehicle.vehicleCatalog.vehicleVariant.VehicleVariantEntity;
+import com.PascuanSilvestre.TorqTrace.features.vehicle.vehiclePowerTrain.engine.EngineEntity;
+import com.PascuanSilvestre.TorqTrace.features.vehicle.vehiclePowerTrain.transmission.TransmissionEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -17,6 +20,7 @@ import lombok.experimental.SuperBuilder;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -26,37 +30,51 @@ import java.util.UUID;
 @SuperBuilder
 @AllArgsConstructor
 @NoArgsConstructor
-public class VehicleEntity  extends AuditableBase {
+public class VehicleConfigurationEntity extends AuditableBase {
 
     @JdbcTypeCode(SqlTypes.VARCHAR)
     @Column(name = "public_id", nullable = false, unique = true, updatable = false, length = 36)
     private UUID publicId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
     @JoinColumn(name="vehicle_brand_id", nullable = false)
     private VehicleBrandEntity vehicleBrand;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY,optional = false)
     @JoinColumn(name="vehicle_model_id", nullable = false)
     private VehicleModelEntity vehicleModel;
 
+     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="vehicle_generation_id" )
+    private VehicleGenerationEntity vehicleGeneration;
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="vehicle_variant_id", nullable = true)
+    @JoinColumn(name="vehicle_variant_id")
     private VehicleVariantEntity vehicleVariant;
 
      @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="vehicle_generation_id", nullable = true)
-    private VehicleGenerationEntity vehicleGeneration;
-
-     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="vehicle_equipment_level_id", nullable = true)
+    @JoinColumn(name="vehicle_equipment_level_id")
     private VehicleEquipmentLevelEntity vehicleEquipmentLevel;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "vehicle_body_type_id", length = 50)
-    private VehicleBodyType vehicleBodyType;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "engine_id")
+    private EngineEntity engine;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "transmission_id")
+    private TransmissionEntity transmission;
+
 
     @Enumerated(EnumType.STRING)
     @Column(name = "vehicle_category_id", length = 50)
     private VehicleCategory vehicleCategory;
+
+
+
+
+    @OneToMany(mappedBy = "vehicleConfiguration",fetch= FetchType.LAZY)
+    private List<UserVehicleEntity> userVehicles;
+
 }
