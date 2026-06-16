89b332ed07580875f3578a40165763a4bc1af408
JoeL2481 <joel248@hotmail.com.ar>
2026-06-16 05:20:22 -0300

    wip

--- src/main/java/com/PascuanSilvestre/TorqTrace/features/userVehicle/maintenance/dto/MaintenanceCreateDTO.java
+++ src/main/java/com/PascuanSilvestre/TorqTrace/features/userVehicle/maintenance/dto/MaintenanceCreateDTO.java
@@ -1,7 +1,9 @@
 package com.PascuanSilvestre.TorqTrace.features.userVehicle.maintenance.dto;
 
 import com.PascuanSilvestre.TorqTrace.features.userVehicle.enums.MaintenanceType;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,8 +18,14 @@ public class MaintenanceCreateDTO {
     @NotNull(message = "Maintenance type is required")
     private MaintenanceType maintenanceType;
 
+    @Size(max = 500, message = "Description can have at most 500 characters")
     private String description;
+
+    @Min(value = 1, message = "Service km must be greater than zero")
     private int serviceKm;
+
+    @Min(value = 0, message = "Next service km cannot be negative")
     private int nextServiceKm;
+
     private Date nextServiceDate;
 }
